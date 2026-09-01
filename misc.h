b46a7ba494d35158b7c23456d3a04a1aff0d8060
Patesz22 <tothpatrik0506@gmail.com>
2026-09-01 11:56:36 +0200

    forgot to include this in prev commit

--- misc.h
+++ misc.h
@@ -1,10 +1,13 @@
 #pragma once
-#include "main.h"
+#include <cstdint>
 
 void PressKey(char key);
 
-bool SafeRead4Bytes(uintptr_t address, int32_t* outInt, float* outFloat);
-bool SafeCopyMemory(void* destination, void* source, size_t size);
+namespace MemoryHelpers
+{
+    bool SafeRead4Bytes(uintptr_t address, int32_t* outInt, float* outFloat);
+    bool SafeCopyMemory(void* destination, void* source, size_t size);
+}
 
 void InspectUIWidgets();
 
